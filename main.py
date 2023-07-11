4e0fa88905b76aa47c3ee5584d6fae6a0a48dff0
Alexandra Sazhaeva <alexa.sazhaeva@gmail.com>
2023-07-11 12:16:35 +0200

    Added functions for filtering and sorting data

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from utils import operations
+
+
 def main():
     data = operations.read_json()
+    data = operations.filter_data(data)
+    data = operations.sort_data(data)
 
 
 if __name__ == '__main__':
--- utils/operations.py
+++ utils/operations.py
@@ -1,10 +1,12 @@
 import json
+from datetime import datetime
 
 
 def read_json():
     """
     Функция считывает данные из json файла и возвращает их
     в качестве списка словарей
+    :return: operations: считаный список словарей
     """
 
     with open("operations.json", "r") as file:
@@ -12,3 +14,39 @@ def read_json():
         operations = json.loads(operations_json)
 
     return operations
+
+
+def filter_data(array):
+    """
+    Каждый словарь обязательно должен содержать информацию о дате перевода,
+    описание перевода, откуда и куда был сделан перевод, должна содержаться сумма
+    перевода и валюта. Некоторые словари содержат недостаточно информации.
+    Необходимо удалить такие словари из списка. Также нужно оставить только
+    записи со статусом EXECUTED.
+    :param array: список словарей
+    :return: filtered_array: отфильтрованный список
+    """
+
+    required_data = ["date", "description", "from", "to", "operationAmount"]
+    filtered_array = []
+
+    for row in array:
+
+        # Проверяем, что словарь не пустой
+        if row:
+
+            # Проверяем, что в словаре содержатся все необходимые данные
+            if row["state"] == "EXECUTED" and all(x in row for x in required_data):
+                filtered_array.append(row)
+
+    return filtered_array
+
+def sort_data(array):
+    """
+    Функция принимает на вход список словарей и сортирует их по дате
+    :param array: список словарей
+    :return: sorted_array: отсортированный список
+    """
+
+    sorted_array = sorted(array, key=lambda x: x['date'], reverse=True)
+    return sorted_array
